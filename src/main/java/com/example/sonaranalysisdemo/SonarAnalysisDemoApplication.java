010d728e319e7e960a3a69513d8829274ce1df32
Gimena Ccoracc Quispe <111783689+GimenaCcoracQuispe@users.noreply.github.com>
2025-04-04 17:48:33 -0500

    Update SonarAnalysisDemoApplication.java

--- src/main/java/com/example/sonaranalysisdemo/SonarAnalysisDemoApplication.java
+++ src/main/java/com/example/sonaranalysisdemo/SonarAnalysisDemoApplication.java
@@ -6,16 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootTest
 class SonarAnalysisDemoApplicationTests {
 
-    /**
-     * Este método es una prueba de contexto para asegurarse de que la aplicación se carga correctamente.
-     * Actualmente está vacío, pero es útil para verificar la carga del contexto de Spring Boot.
-     * 
-     * Opciones para completar este método:
-     * 1. Implementar una prueba real que verifique si el contexto de la aplicación se carga correctamente.
-     * 2. Lanzar una excepción UnsupportedOperationException si la prueba no debe implementarse aún.
-     */
     @Test
     void contextLoads() {
-        // Este método puede ser completado con pruebas de contexto o lanzando una excepción si aún no se implementa.
+
     }
 }
